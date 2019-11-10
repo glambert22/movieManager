8866dd2105880e757700266d5e9aa58375f90d81
Gregory Lambert <gregory.lambert@gmail.com>
2019-11-10 11:29:14 -0600

    restore jsonutils

--- src/main/java/com/secureally/demo/utils/JsonUtils.java
+++ src/main/java/com/secureally/demo/utils/JsonUtils.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.MissingNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.javers.core.Javers;
 import org.javers.core.JaversBuilder;
 import org.javers.core.diff.Diff;
